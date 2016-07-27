4be07b45606d53a89366ba59bc2262733dad12ba
dixonqin <dixonqin@yeah.net>
2016-07-27 13:51:18 +0800

    by Dixon
    add user info, add info_view page, add headImg function(have not
    completly test) add orderForm manage function

--- shoppingnet/customauth/models.py
+++ shoppingnet/customauth/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser
@@ -45,6 +46,7 @@ class MyUser(AbstractBaseUser):
     )
     is_shop = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_complete = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
 
     objects = MyUserManager()
--- shoppingnet/shopping/models.py
+++ shoppingnet/shopping/models.py
@@ -3,8 +3,12 @@ from django.conf import settings
 
 # Create your models here.
 class Shop(models.Model):
-	name = models.CharField(max_length = 30)
-	category = models.CharField(max_length = 30)
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default='')
+	name = models.CharField(max_length = 30, default="")
+	description = models.CharField(max_length = 200, default="")
+	category = models.CharField(max_length = 20, default="")
+	#location = models.CharField(max_length = 100, default="")
+	address = models.CharField(max_length = 200, default="")
 
 	class Meta:
 		ordering = ["-name"]
@@ -12,39 +16,58 @@ class Shop(models.Model):
 	def __str__(self):
 		return self.name
 
-# class User(models.Model):
-# 	name = models.CharField(max_length = 30)
-# 	email = models.EmailField()
-
-# 	def __str__(self):
-# 		return self.name
-
-class Goods(models.Model):
-	name = models.CharField(max_length = 30)
-	shop = models.ForeignKey(Shop, on_delete = models.CASCADE)
-	price = models.IntegerField(default = 0)
+class Customer(models.Model):
+	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, default = '')
+	name = models.CharField(max_length = 20, default="")
+	address = models.CharField(max_length = 200, default="")
+	sex = models.CharField(max_length = 10, default="")
+	tel = models.CharField(max_length = 20, default = "")
+	#head_img = models.ImageField(upload_to = 'img', null=True, blank=True)
+	self_intro = models.CharField(max_length = 300, default="")
 
 	def __str__(self):
 		return self.name
 
+
+class Goods(models.Model):
+	shop = models.ForeignKey(Shop, on_delete = models.CASCADE, default = '')
+	#以下商品信息，店主无法直接修改
+	name = models.CharField(max_length = 30, default = "")
+	category = models.CharField(max_length = 20, default="")
+	description = models.CharField(max_length = 200, default="")
+	price = models.IntegerField(default = 0)
+	number = models.IntegerField(default = 0)
+	#以下信息，店主无法修改
+	sold_number = models.IntegerField(default = 0)
+	rating = models.IntegerField(default = 5, editable = 'false')
+	rating_number = models.IntegerField(default = 0, editable = 'false')
+
+	def __str__(self):
+		return str(self.name)+'+'+str(self.id)
+
 class Comment(models.Model):
 	rating = models.IntegerField(default = 0)
-	content = models.TextField(max_length = 200)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-	goods = models.ForeignKey(Goods, on_delete = models.CASCADE)
+	content = models.TextField(max_length = 200, default = "")
+	customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, default = '')
+	goods = models.ForeignKey(Goods, on_delete = models.CASCADE, default = '')
 
 	def __str__(self):
 		return self.content
 
 class ShoppingCart(models.Model):
 	number = models.IntegerField(default = 0)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-	goods = models.ForeignKey(Goods, on_delete = models.CASCADE)
+	customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, default = "")
+	goods = models.ForeignKey(Goods, on_delete = models.CASCADE, default = "")
 
 class OrderForm(models.Model):
+	status = models.IntegerField(default = 0)#1是下单，2是确认，3是发货，4是收货
 	number = models.IntegerField(default = 0)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-	goods = models.ForeignKey(Goods, on_delete = models.CASCADE)
+	customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, default="")
+	goods = models.ForeignKey(Goods, on_delete = models.CASCADE, default = "")
+	message = models.CharField(max_length = 200, default = "")
+	tel = models.CharField(max_length = 20, default = "")
+	address = models.CharField(max_length = 100, default = "")
 
 	def __str__(self):
-		return self.user.username
+		return self.customer.name
+
