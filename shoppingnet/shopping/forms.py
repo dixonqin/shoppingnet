2fd72d2cdb78867a5e6ef07f8d46f057210e4768
dixonqin <dixonqin@yeah.net>
2016-07-28 15:00:59 +0800

    by Dixon
    整合代码，search功能  评论交互功能基本完成，现需要进行进一步完善基本功能的细节

--- shoppingnet/shopping/forms.py
+++ shoppingnet/shopping/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from registration.forms import RegistrationForm
 from customauth.models import MyUser
-from shopping.models import Goods, Shop, Customer
+from shopping.models import Goods, Shop, Customer, OrderForm, Comment, Search, Reply
 from django.utils.translation import ugettext_lazy as _
 
 class MyCustomUserForm(RegistrationForm):
@@ -40,23 +40,6 @@ class ManageGoodsForm(forms.ModelForm):
 
 		# }
 
-class CommentForm(forms.Form):
-	rating = forms.IntegerField()
-	content = forms.CharField()
-
-class CustomerForm(forms.ModelForm):
-	class Meta:
-		model = Customer
-		exclude = ['user',]
-		labels = {
-			#'head_img':_("头像"),
-			'name':_("姓名"),
-			'sex':_("性别"),
-			'tel':_("联系电话"),
-			'address':_("地址"),
-			'self_intro':_("个人简介"),
-		}
-
 class ShopInfoForm(forms.ModelForm):
 	class Meta:
 		model = Shop
@@ -65,5 +48,52 @@ class ShopInfoForm(forms.ModelForm):
 			'name':_("名称"),
 			'category':_("类别"),
 			'description':_("描述"),
+			'tele':_("联系方式"),
 		}
 
+class CustomerForm(forms.ModelForm):
+	class Meta:
+		model = Customer
+		exclude = ['user',]
+		labels = {
+			'head_img':_("头像"),
+			'name':_("姓名"),
+			'sex':_("性别"),
+			'self_intro':_("个人简介"),
+			'address':_("地址"),
+			'tele':_("联系方式")
+		}
+
+class UserOrderForm(forms.ModelForm):
+	class Meta:
+		model = OrderForm
+		fields = ['number', 'message', 'tele', 'address', ]
+		labels = {
+			'number':_("数量"),
+			'message':_("附加信息"),
+			'tele':_("联系方式"),
+			'address':_("收货地址"),
+		}
+
+class CommentForm(forms.ModelForm):
+	class Meta:
+		model = Comment
+		fields = ['rating', 'content',]
+		labels = {
+			'rating':_("评分"),
+			'content':_('评价'),
+		}
+
+class ReplyForm(forms.ModelForm):
+	class Meta:
+		model = Reply
+		fields = ['rating', 'content',]
+		labels = {
+			'rating':_("评分"),
+			'content':_('回复内容'),
+		}
+
+class SearchForm(forms.ModelForm):
+	class Meta:
+		model = Search
+		fields = ['content', 'field_name', 'object_name']
--- shoppingnet/shoppingnet/settings.py
+++ shoppingnet/shoppingnet/settings.py
@@ -129,5 +129,6 @@ LOGIN_REDIRECT_URL = '/'
 
 AUTH_USER_MODEL = 'customauth.MyUser'
 
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
